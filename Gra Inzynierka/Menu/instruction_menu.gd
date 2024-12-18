6cf69e2e303911064ba2f023d5c707c5108f059c
KamilGrzonka <kamilgrzonka@interia.pl>
2024-12-18 20:07:16 +0100

    Generowanie przeszkód

--- Gra Inzynierka/Menu/instruction_menu.gd
+++ Gra Inzynierka/Menu/instruction_menu.gd
@@ -5,7 +5,7 @@ extends Control
 func _ready():
 	back_button.pressed.connect(self._on_back_button_pressed)
 	$RichTextLabel.bbcode_enabled = true
-	$RichTextLabel.bbcode_text = "[center]Pokonaj nadciągające fale wrogów i przetrwaj jak najdłużej. Zdobywał punkty doświadczenia by ulepszać postać. Steruj postacią przy pomocy klawiszy WSAD i myszki.[/center]"
+	$RichTextLabel.bbcode_text = "[center]Pokonaj nadciągające fale wrogów i przetrwaj jak najdłużej. Steruj postacią przy pomocy klawiszy WSAD. Unikaj pułapek i przeszkód. By wygrać musisz pokonać finalnego boss-a.[/center]"
 
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://Menu/main_menu.tscn")
--- Gra Inzynierka/World/terrain.gd
+++ Gra Inzynierka/World/terrain.gd
@@ -33,76 +33,82 @@ func spawn_chunk(chunk_key: Vector2):
 	var rng = RandomNumberGenerator.new()
 	rng.seed = hash(chunk_key) # Deterministyczne losowanie na podstawie pozycji chunku
 
-	# Sprawdź, czy ma pojawić się trawa czy pułapka (10% szans na pułapkę)
-	var is_trap = rng.randi_range(1, 100) <= 2 # 10% szansy na pułapkę
+	var is_trap = rng.randi_range(1, 100) <= 1 # 1% szansy na pułapkę
+	var is_grass = not is_trap
 
 	if is_trap:
-		# Ładowanie pułapki
-		var trap_texture_path = "res://Textures/Traps/trap.png"
-		var texture = load(trap_texture_path)
-
-		# Debugowanie
-		if texture == null:
-			print("Nie znaleziono tekstury pułapki:", trap_texture_path)
-		else:
-			print("Generowanie pułapki:", trap_texture_path)
-
-		# Ustawienie tekstury pułapki
-		var sprite = chunk.get_node("Sprite2D")
-		if sprite:
-			sprite.texture = texture
-			sprite.scale = Vector2(0.5, 0.5) # Skalowanie tekstury
-			sprite.centered = true # Wyśrodkowanie tekstury
-			sprite.region_enabled = true # Włączenie regionu tekstury
-			sprite.region_rect = Rect2(0, 0, 32, 32) # Przycinanie tekstury
-		else:
-			print("Nie znaleziono Sprite2D w chunku!")
-
-	# Dodanie CollisionShape dla obszaru pułapki
-		var trap_area = Area2D.new()
-		trap_area.name = "TrapArea"
-		var trap_collision = CollisionShape2D.new()
-		var trap_shape = RectangleShape2D.new()
-		trap_shape.size = Vector2(16, 16) # Możesz dostosować wielkość
-		trap_collision.shape = trap_shape
-		trap_area.add_child(trap_collision)
-
-		chunk.add_child(trap_area)
-
-		# Dodajemy detekcję kolizji
-		trap_area.connect("body_entered", Callable(self, "_on_trap_area_entered"))
-		
-	else:
-		# Generowanie standardowej trawy
-		var texture_index = rng.randi_range(1, 10)
-		var texture_path = "res://Textures/Grass/grass" + str(texture_index) + ".png"
-		var texture = load(texture_path)
-
-		# Debugowanie
-		if texture == null:
-			print("Nie znaleziono tekstury:", texture_path)
-		else:
-			print("Ładowanie trawy:", texture_path)
-
-		# Ustawienie tekstury
-		var sprite = chunk.get_node("Sprite2D")
-		if sprite:
-			sprite.texture = texture
-		else:
-			print("Nie znaleziono Sprite2D w chunku!")
+		generate_trap(chunk)
+	elif is_grass:
+		generate_grass(chunk, rng)
+		# Dodaj przeszkody na trawie z szansą 2%
+		if rng.randi_range(1, 100) <= 2:
+			generate_obstacle(chunk, rng)
 
 	# Dodanie chunku do świata
 	add_child(chunk)
 	active_chunks[chunk_key] = chunk
 
+func generate_trap(chunk):
+	var trap_texture_path = "res://Textures/Traps/trap.png"
+	var texture = load(trap_texture_path)
+
+	var sprite = chunk.get_node("Sprite2D")
+	if sprite:
+		sprite.texture = texture
+		sprite.scale = Vector2(0.5, 0.5) # Skalowanie tekstury
+		sprite.centered = true # Wyśrodkowanie tekstury
+		sprite.region_enabled = true # Włączenie regionu tekstury
+		sprite.region_rect = Rect2(0, 0, 32, 32) # Przycinanie tekstury
+
+	var trap_area = Area2D.new()
+	trap_area.name = "TrapArea"
+	var trap_collision = CollisionShape2D.new()
+	var trap_shape = RectangleShape2D.new()
+	trap_shape.size = Vector2(16, 16)
+	trap_collision.shape = trap_shape
+	trap_area.add_child(trap_collision)
+
+	trap_area.connect("body_entered", Callable(self, "_on_trap_area_entered"))
+	chunk.add_child(trap_area)
+
+func generate_grass(chunk, rng):
+	var texture_index = rng.randi_range(1, 10)
+	var texture_path = "res://Textures/Grass/grass" + str(texture_index) + ".png"
+	var texture = load(texture_path)
+	var sprite = chunk.get_node("Sprite2D")
+	if sprite:
+		sprite.texture = texture
+
+func generate_obstacle(chunk, rng):
+	var obstacle_index = rng.randi_range(1, 4) # Wybierz losowy plik przeszkody od 1 do 4
+	var obstacle_path = "res://Textures/Obstacles/obstacle" + str(obstacle_index) + ".png"
+	var texture = load(obstacle_path)
+
+	var obstacle_sprite = Sprite2D.new()
+	obstacle_sprite.texture = texture
+	obstacle_sprite.position = Vector2(0, 0)
+	obstacle_sprite.centered = true
+
+	# Dodaj kolizję
+	var static_body = StaticBody2D.new()
+	var collision_shape = CollisionShape2D.new()
+	var shape = RectangleShape2D.new()
+	shape.size = Vector2(16, 16)
+	collision_shape.shape = shape
+
+	static_body.add_child(collision_shape)
+	static_body.position = obstacle_sprite.position
+
+	chunk.add_child(obstacle_sprite)
+	chunk.add_child(static_body)
+
 func remove_chunk(chunk_key: Vector2):
 	if active_chunks.has(chunk_key):
 		active_chunks[chunk_key].queue_free()
 		active_chunks.erase(chunk_key)
-		
-# Funkcja, która będzie wywoływana, gdy gracz wejdzie w pułapkę
+
 func _on_trap_area_entered(body):
-	if body.is_in_group("Player"):  # Sprawdzamy, czy to gracz
-		var damage = 10  # Możesz ustawić dowolną wartość obrażeń
-		body._on_hurtbox_hurt(damage, Vector2.ZERO, 0)  # Zadanie obrażeń graczowi
+	if body.is_in_group("Player"):
+		var damage = 10
+		body._on_hurtbox_hurt(damage, Vector2.ZERO, 0)
 
